be7c9dab8ff12ff0e46754ec81c0249e5dc96310
Gus Beare <gus@carawaydesign.com>
2016-12-08 13:54:40 +0000

    added assembly details

---
+++ NancyUserManager/Properties/AssemblyInfo.cs
@@ -0,0 +1,32 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Nancy User Manager")]
+[assembly: AssemblyDescription("simple web  site for user management")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Caraway Design")]
+[assembly: AssemblyProduct("NancyUserManager")]
+[assembly: AssemblyCopyright("Caraway Design 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8990a28d-3962-4832-94b3-538c97ef997a")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
